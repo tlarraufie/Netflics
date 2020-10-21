8a648c3dd11017fefc0287e9ab31cdafb578a592
tom <tom.larraufie@cpe.fr>
2020-10-21 20:09:43 +0200

    template feature

--- api/apis/films.php
+++ api/apis/films.php
@@ -19,11 +19,10 @@
     }
 
     function getAllFavoris($dbh){
-
+        
         try{
             //$sql = "SELECT * FROM Listes WHERE idUser = '".$_SESSION['idUser']."' ";
             $sql = "SELECT * FROM Listes L JOIN Films F ON L.idFilm = F.idFilm WHERE idUser = '".$_SESSION['idUser']."' ";
-
             $sth = $dbh->prepare( $sql );
             $sth->execute();
             $data = $sth->fetchAll(PDO::FETCH_OBJ);
@@ -36,6 +35,25 @@
         }
     }
 
+    function addFavoris($dbh,$idFilm){
+
+        try{
+            
+            $sql = "INSERT INTO Listes (idUser,idFilm,date) VALUES ('".$_SESSION['idUser']."','".$idFilm."','04/09/01');";
+            $sth = $dbh->prepare( $sql );
+            $sth->execute();
+
+            $data = [$sth->fetchAll(PDO::FETCH_OBJ),"REUSSI"];
+            
+        }
+        catch (PDOException $e){
+            echo '<pre>';
+            var_dump($e);
+            $test = "PERDU";
+        }
+        return $data;
+    }
+
 
    
     if($_SERVER["REQUEST_METHOD"]=="GET"){
@@ -45,6 +63,13 @@
             if(isset($_GET['favoris'])){
                 if($_GET['favoris']==true)
                 $data = getAllFavoris($dbh);
+
+            }elseif(isset($_GET['x'])){
+                $idFilm = $_GET['x'];
+                $data = $idFilm;
+
+                addFavoris($dbh,$idFilm);
+
             }else{
                 $data = getAllFilms($dbh);
             }
@@ -58,7 +83,31 @@
             die('could not connect to database');
             display404();
         }        
-    }else{
+    }elseif($_SERVER["REQUEST_METHOD"]=="POST"){
+        // try {
+
+        //     if(isset($_POST['x']))
+        //     {
+        //         // $data=addFavoris($dbh);
+        //         $data = $_POST['x'];
+                
+        //     }else{
+        //         $data = "hello";
+        //     }
+
+        //     header('Content-Type: application/json');
+        //     echo json_encode($data);
+    
+        // } catch (PDOException $e) {
+        //     echo '<pre>';
+        //     var_dump($e);
+        //     die('could not connect to database');
+        //     display404();
+        // } 
+        display404();
+        
+    }
+    else{
         display404();
     }         
        
